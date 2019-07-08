6bae1f43ccbe886d9e469097315c1f7389c3455a
Pedro Mantica <pedro.mantica@kensho.com>
2019-07-08 13:33:48 -0400

    Moved print_query to emit_sql.py

--- graphql_compiler/tests/integration_tests/integration_test_helpers.py
+++ graphql_compiler/tests/integration_tests/integration_test_helpers.py
@@ -4,10 +4,7 @@ from decimal import Decimal
 import six
 
 from ... import graphql_to_match, graphql_to_sql
-from ...compiler.ir_lowering_sql.metadata import SqlMetadata
-from sqlalchemy.dialects.mssql import UNIQUEIDENTIFIER, BIT
-from ...compiler.helpers import GraphQLCompilationError
-from datetime import date, datetime
+from ...compiler.emit_sql import print_query
 from sqlalchemy.dialects import mssql
 
 
@@ -73,72 +70,3 @@ def compile_and_run_sql_query(schema, graphql_query, parameters, engine, sql_met
                              'to be the same.')
     return results_with_query_string
 
-
-def print_query(statement, dialect):
-    """
-    Print a query, with values filled in for debugging purposes *only* for security, you should
-    always separate queries from their values. Please also note that this function is quite slow.
-    Inspiration from:
-    https://stackoverflow.com/questions/5631078/sqlalchemy-print-the-actual-query/5698357
-    """
-    compiler = statement._compiler(dialect)
-    class LiteralCompiler(compiler.__class__):
-        def visit_bindparam(
-                self, bindparam, within_columns_clause=False,
-                literal_binds=False, **kwargs
-        ):
-            return super(LiteralCompiler, self).render_literal_bindparam(
-                    bindparam, within_columns_clause=within_columns_clause,
-                    literal_binds=literal_binds, **kwargs
-            )
-
-        def render_literal_bindparam(self, bindparam, **kw):
-            value = bindparam.effective_value
-            if isinstance(value, list):
-                for sub_value in value:
-                    if isinstance(sub_value, list):
-                        raise GraphQLCompilationError('Param {} is a nested list. No nested lists '
-                                                      'allowed'.format(bindparam.key))
-                    if not isinstance(sub_value, bindparam.type.python_object):
-                        raise GraphQLCompilationError('Param {} is a list with a value {} that is '
-                                                      'not of the expected type {}.'
-                                                      .format(bindparam.key, sub_value,
-                                                              str(bindparam.type.python_object)))
-            else:
-                # This SQLAlchemy type does not have a python_type implementation.
-                if isinstance(bindparam.type, UNIQUEIDENTIFIER):
-                    if not isinstance(value, str):
-                        raise GraphQLCompilationError('Param {} is not of the expected type {}.'
-                                                      .format(value, str))
-                # This SQLAlchemy type does not have a python_type implementation.
-                elif isinstance(bindparam.type, BIT):
-                    if not isinstance(value, int):
-                        raise GraphQLCompilationError('Param {} is not of the expected type {}.'
-                                                      .format(value, int))
-                elif not isinstance(value, bindparam.type.python_object):
-                    raise GraphQLCompilationError('Param {} is not of the expected type {}.'
-                          .format(value, str(bindparam.type.python_object)))
-            return self.render_literal_value(value, bindparam.type)
-
-        def render_literal_value(self, value, type_):
-            if isinstance(value, (list, tuple)):
-                return "(%s)" % (",".join([self.render_literal_value(x, type_) for x in value]))
-            else:
-                if isinstance(value, bool):
-                    return '1' if value else '0'
-                elif isinstance(value, (int, float, Decimal)):
-                    return str(value)
-                elif isinstance(value, str):
-                    return "'%s'" % value.replace("'", "''")
-                elif isinstance(value, datetime):
-                    return "{ts '%04d-%02d-%02d %02d:%02d:%02d.%03d'}" % (
-                        value.year, value.month, value.day,
-                        value.hour, value.minute, value.second,
-                        value.microsecond / 1000)
-                elif isinstance(value, date):
-                    return "{d '%04d-%02d-%02d'} " % (
-                        value.year, value.month, value.day)
-            return super(LiteralCompiler, self).render_literal_value(value, type_)
-
-    compiler = LiteralCompiler(dialect, statement)
-    return str(compiler.process(statement))
